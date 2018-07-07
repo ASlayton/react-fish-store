1b1cec3c1bd35fbff2b4f6541bc5cbb07071ce6c
Amanda Slayton <alslayton86@gmail.com>
2018-07-07 13:10:02 -0500

    user is able to login

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // import Fish from '../components/Fish/fish';
 import Home from '../components/Home/home';
 import Inventory from '../components/Inventory/inventory';
-// import Login from '../components/Login/login';
+import Login from '../components/Login/login';
 import Navbar from '../components/Navbar/navbar';
 // import New from '../components/New/new';
 // import Order from '../components/Order/order';
@@ -65,13 +65,13 @@ class App extends React.Component {
                   <Route path="/" exact component={Home} />
                   <PrivateRoute path="/inventory" authed={this.state.authed} component={Inventory} />
                   <PublicRoute path="/register" authed={this.state.authed} component={Register} />
+                  <PublicRoute path="/login" authed={this.state.authed} component={Login} />
                 </Switch>
               </div>
             </div>
           </div>
         </BrowserRouter>
-        {/* <Fish /> */}
-        {/* <Login />
+        {/* <Fish />
         <New />
         <Order />
         <OrderSpa />
