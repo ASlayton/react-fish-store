495d1597e745c40cf9b2db23935de0b1fef57b3e
Amanda Slayton <alslayton86@gmail.com>
2018-07-09 21:01:36 -0500

    user order heading shows up

--- src/components/OrderSpa/orderSpa.js
+++ src/components/OrderSpa/orderSpa.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 import orderRequests from '../../firebaseRequests/orders';
 import authRequests from '../../firebaseRequests/auth';
 import './OrderSpa.css';
@@ -22,13 +24,19 @@ class OrderSpa extends React.Component {
   render () {
     const orderComponents = this.state.orders.map((order) => {
       return (
-        <button>{order.id}</button>
+        <button
+          key={order.id}
+          className="col-xs-12 btn btn-default order-button"
+        >
+          <span className="col-xs-6">OrderNumber: {order.id}</span>
+          <span className="col-xs-6">Order Date: {moment(order.dateTime).format('LLL')}</span>
+        </button>
       );
     });
     return (
-      <div className="OrderSpa">
+      <div className="OrderSpa col-xs-12">
         <h2>Orders</h2>
-        <button>New Order</button>
+        <button><Link to="/new">New Order</Link></button>
         <ul>
           {orderComponents}
         </ul>
